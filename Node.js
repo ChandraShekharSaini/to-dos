9b340337c6f26be97f6def4ebfa75c3be0aad091
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-05-20 18:03:03 +0530

    Added

---
+++ Node.js
@@ -0,0 +1,24 @@
+"Node js";
+// Express
+
+// middlewars
+
+// sessions
+
+// cookies
+
+// cors()
+
+// jwt
+
+// cookie-parser()
+
+// passport js
+
+// MySql Connectivity
+
+// MongoDb Conectivity
+
+// routes , controller
+
+// deployment using render
---
+++ Rect.js
@@ -0,0 +1,44 @@
+"Rect Js"
+
+// props
+
+// Hooks
+ 
+// useState()
+
+// useEffect()
+
+// useCallback()
+
+// useOptimistic()
+
+// useTransition()
+
+// useId()
+
+// useMemo()
+
+// useActionState()
+
+// useTranition()
+
+// React.Memo()
+
+// Lazy Loading
+
+// Suspence
+
+
+
+// Context API
+
+// Redux Toolkit
+
+// How to make React
+// use dependencies babel.js and webpack.js
+
+// Pure Components Vs ImpureComponents
+
+// Function Vs Class Components
+
+// Custom Hooks
